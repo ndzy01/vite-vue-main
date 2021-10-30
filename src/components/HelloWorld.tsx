591a12d193ad757ab082f9f4d4c4f50002ad845f
张一 <2326962137@qq.com>
2021-10-30 08:21:06 +0800

    chore prettier

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,13 +1,13 @@
 import { ref, withModifiers, defineComponent } from 'vue';
-import './HelloWorld.css'
+import './HelloWorld.css';
 
 export default defineComponent({
   props: {
-    msg: String
+    msg: String,
   },
 
   setup(props) {
-    const count = ref(0)
+    const count = ref(0);
     const { msg } = props;
 
     const inc = () => {
@@ -16,31 +16,41 @@ export default defineComponent({
 
     return () => (
       <>
-        <h1>{ msg }</h1>
+        <h1>{msg}</h1>
 
         <p>
           Recommended IDE setup:
-          <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+          <a href="https://code.visualstudio.com/" target="_blank">
+            VSCode
+          </a>
           +
-          <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+          <a href="https://github.com/johnsoncodehk/volar" target="_blank">
+            Volar
+          </a>
         </p>
 
-        <p>See <code>README.md</code> for more information.</p>
+        <p>
+          See <code>README.md</code> for more information.
+        </p>
 
         <p>
           <a href="https://vitejs.dev/guide/features.html" target="_blank">
             Vite Docs
           </a>
           |
-          <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+          <a href="https://v3.vuejs.org/" target="_blank">
+            Vue 3 Docs
+          </a>
         </p>
 
-        <button type="button" onClick={withModifiers(inc, ["self"])}>count is:{count.value}</button>
+        <button type="button" onClick={withModifiers(inc, ['self'])}>
+          count is:{count.value}
+        </button>
         <p>
           Edit
           <code>components/HelloWorld.tsx</code> to test hot module replacement.
         </p>
       </>
-    )
-  }
-})
+    );
+  },
+});
